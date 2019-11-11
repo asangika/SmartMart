4a0dd05cbd87b907cfb8b83a18990380ce0d36a8
Deepthashash <shashideepshashi@outlook.com>
2019-11-11 09:48:34 +0530

    Integrated firebase with angular

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAB_utLcQYQIF4qI-00E6eGZXxCU5zwqz4",
+    authDomain: "smartshopping-b4b4b.firebaseapp.com",
+    databaseURL: "https://smartshopping-b4b4b.firebaseio.com",
+    projectId: "smartshopping-b4b4b",
+    storageBucket: "smartshopping-b4b4b.appspot.com",
+    messagingSenderId: "66425334182",
+    appId: "1:66425334182:web:e83df85a1e4a9307710767"
+  }
 };
 
 /*
